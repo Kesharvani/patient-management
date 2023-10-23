74257b424c1e6ded2e44cd1cf61ddbd429463c5a
Kesharvani <shubhamkesharvani888@gmail.com>
2023-10-24 01:29:07 +0530

    feat:added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import "./App.css";
+import { FaHospitalSymbol } from "react-icons/fa";
+import { NavLink, Routes, Route } from "react-router-dom";
 function App() {
+  const style = ({ isActive, isPending }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "#002884" : "black",
+    };
+  };
   return (
     <>
-      <div>this is app</div>
+      <div className="App">
+        <nav className="nav">
+          <h3 className="patient-heading">
+            <NavLink
+              to="/"
+              style={{ ...style, color: "#002884", textDecoration: "none" }}
+            >
+              Patient Management System
+            </NavLink>
+          </h3>
+          <NavLink to="/" style={{ ...style, color: "#002884" }}>
+            <FaHospitalSymbol size={30} className="hospital-icon" />
+          </NavLink>
+          <div className="navigations">
+            <NavLink to="/" style={style}>
+              Patient
+            </NavLink>
+            <NavLink to="/teacher" style={style}>
+              Ward
+            </NavLink>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<div>Testing</div>} />
+        </Routes>
+      </div>
     </>
   );
 }
